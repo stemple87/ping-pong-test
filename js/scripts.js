ba88be7135abf8d5bdb5f4cc289838ecfbe17774
simon <simonctemple@live.com>
2016-01-22 15:21:57 -0800

    added pingPongTester variable to make sure tests still pass

--- js/scripts.js
+++ js/scripts.js
@@ -23,3 +23,23 @@ $(document).ready(function() {
   $("div#hiddenResult").show();
   });
 });
+
+var pingPongTester = function(input) {
+  if (input % 15 === 0) {
+    input = "";
+    input += "PingPong";
+    return input;
+  } else if (input % 3 === 0) {
+    input = "";
+    input += "Ping";
+    return input;
+  } else if (input % 5 === 0) {
+    input = "";
+    input += "Pong";
+    return input;
+  } else {
+    input = "";
+    input += "Not 3, 5, or 15";
+    return input;
+  }
+}
