6fdfda30a0670179330e1e8c45b19a2bfce99501
simon <simonctemple@live.com>
2016-01-22 08:39:00 -0800

    create functionality for 5. if input is 5 number is replaced by Pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,11 @@
 var pingPong = function(input) {
   if (input === 3) {
-    input = ""
+    input = "";
     input += "Ping";
+    return input;
+  } else if (input === 5) {
+    input = "";
+    input += "Pong";
     return input;
   }
 }
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('pingPong', function() {
   it("will replace any numbers divisible by 3 with the word Ping", function() {
     expect(pingPong(3)).to.equal("Ping");
   });
+  it("will replace any numbers divisible by 5 with the word Pong", function() {
+    expect(pingPong(5)).to.equal("Pong");
+  });
 });
