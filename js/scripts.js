4fba7d761f1229b01beb327bdf76d8298abdb732
simon <simonctemple@live.com>
2016-01-22 09:33:37 -0800

    will fill in index.html page, add a loop in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,38 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var userInput = $("input#pigLatin").val();
+    var result = pigLatin(userInput);
+    $("div#result").text(result);
+    // $("#result").show();
+    // console.log();
+    event.preventDefault();
+  });
+
+});
+
+
+
 var pingPong = function(input) {
-  if (input % 15 === 0) {
-    input = "";
-    input += "PingPong";
-    return input;
-  } else if (input % 3 === 0) {
-    input = "";
-    input += "Ping";
-    return input;
-  } else if (input % 5 === 0) {
-    input = "";
-    input += "Pong";
-    return input;
+  for (var i = 0; i < input.length; i++) {
+
+    var listItem = "<li>"
+    if (i % 15 === 0) {
+      input = "";
+      input += "PingPong";
+      listItem += input + "</li>";
+      // return input;
+    } else if (i % 3 === 0) {
+      input = "";
+      input += "Ping";
+      listItem += input + "</li>";
+      // return input;
+    } else if (i % 5 === 0) {
+      input = "";
+      input += "Pong";
+      listItem += input + "</li>";
+      // return input;
+    } else {
+      listItem += [i] + "</li>";
+    }
   }
 }
